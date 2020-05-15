2e279ed83240084dcb8f9494ab49b1a06579b365
Selawen <37039183+Selawen@users.noreply.github.com>
2020-05-15 16:54:35 +0200

    Added animation to prefab & commented a bit
    - Added animation to the frog (tempPlayer) prefab
    - Put in some comments
    - Made animation even smoother
    - Changed FinishText to TextMapPro for more fancy text options

--- Biofrog Infinite Code/Assets/Game/Scripts/Character_Animation.cs
+++ Biofrog Infinite Code/Assets/Game/Scripts/Character_Animation.cs
@@ -6,22 +6,21 @@ public class Character_Animation : MonoBehaviour
 {
     public Animator Kikker;
     public CharacterController PlayerMovement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //play jump animation when player jumps
         if ((Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1)) && (PlayerMovement.isGrounded))
         {
-            print("jomp");
+            Debug.Log("jomp");
             Kikker.ResetTrigger("SmallJump");
             Kikker.SetTrigger("SmallJump");
-        }
-
-      
+        }      
     }
 }
