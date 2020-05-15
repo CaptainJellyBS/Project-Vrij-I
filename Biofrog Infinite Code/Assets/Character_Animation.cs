07d0f6fd739e8c2b335b8da3a5ecf866ea94b75d
Jesse Blom <jb99ak@gmail.com>
2020-05-15 15:04:57 +0200

    Added Froge animation

---
+++ Biofrog Infinite Code/Assets/Character_Animation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Animation : MonoBehaviour
+{
+    public Animator Kikker;
+    public CharacterController PlayerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && (PlayerMovement.isGrounded))
+        {
+            print("jomp");
+            Kikker.ResetTrigger("SmallJump");
+            Kikker.SetTrigger("SmallJump");
+        }
+
+      
+    }
+}
