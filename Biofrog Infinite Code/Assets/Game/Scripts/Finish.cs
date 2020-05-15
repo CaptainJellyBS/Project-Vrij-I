77daccc65f1555a6bf35bdd4b8acda89562d67e0
Mana <manakouwenberg@gmail.com>
2020-05-15 10:08:42 +0200

    Hacked in a quick change so the level doesn't change on finish if the level string is a specific word, instead showing text this commit message is too long

--- Biofrog Infinite Code/Assets/Game/Scripts/Finish.cs
+++ Biofrog Infinite Code/Assets/Game/Scripts/Finish.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
     public string nextLevel;
+    public GameObject finishTextFuckYouUnity;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(nextLevel);
+            if (nextLevel != "TempEndGame") //uggo magic string so tutorial doesn't crash.
+            { SceneManager.LoadScene(nextLevel); return; }
+
+            finishTextFuckYouUnity.SetActive(true);
         }
     }
 }
