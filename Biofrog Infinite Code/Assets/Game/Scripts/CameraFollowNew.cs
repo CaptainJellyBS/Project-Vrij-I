e31335ef0f3666e81a09240a93c4ac8f487bd658
Mana <manakouwenberg@gmail.com>
2020-05-29 08:29:18 +0200

    Added rotating camera

--- Biofrog Infinite Code/Assets/Game/Scripts/CameraFollowNew.cs
+++ Biofrog Infinite Code/Assets/Game/Scripts/CameraFollowNew.cs
@@ -6,9 +6,48 @@ public class CameraFollowNew : MonoBehaviour
 {
     public Vector3 offset;
     public GameObject player;
+    bool turning;
 
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z) + offset;
+
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        if (turning) { return; }
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            StartCoroutine(CameraRotation(90));
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(CameraRotation(-90));
+        }
+    }
+
+    IEnumerator CameraRotation(float target)
+    {
+        turning = true;
+        float i = 0;
+        float change = 5.0f;
+        while(i<target)
+        {
+            i += change;
+            transform.Rotate(Vector3.up, change, Space.World);
+            offset = Quaternion.AngleAxis(change, Vector3.up) * offset;
+            yield return null;
+        }
+
+        while (i > target)
+        {
+            i -= change;
+            transform.Rotate(Vector3.up, -change, Space.World);
+            offset = Quaternion.AngleAxis(-change, Vector3.up) * offset;
+            yield return null;
+        }
+        turning = false;
     }
 }
