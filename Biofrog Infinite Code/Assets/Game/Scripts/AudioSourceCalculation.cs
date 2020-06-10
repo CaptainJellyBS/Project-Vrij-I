1c836d61bb7136b65bdb46f417609165bffeff98
Mana <manakouwenberg@gmail.com>
2020-06-10 12:03:13 +0200

    Added background noise and pipe noise
    -Added background noise and added audiosource playing it to the Audiolistener
    -Added a child object to pipes that has an audio source and moves itself to the point on the collider bounds that is closest to the player. This isn't a great solution, I think, but the sound was kinda crap otherwise.

---
+++ Biofrog Infinite Code/Assets/Game/Scripts/AudioSourceCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Note: This script could create a lot of lag if there are shittons of pipes in the area, and I don't really know if it's a good idea to have the script in the first place.
+//It also doesn't really work perfectly, because bounding boxes of colliders seem to be quite off, but *shrug*, it works well enough for now
+public class AudioSourceCalculation : MonoBehaviour
+{
+    Collider col;
+    MultipleFrogs frogs;
+    GameObject curFrog;
+
+    void Start()
+    {
+        frogs = FindObjectOfType<MultipleFrogs>();
+        col = GetComponentInParent<Collider>();
+    }
+
+    void Update()
+    {
+        //Set the position of the audiosource to the point on the collider that is closest to the frog
+        curFrog = frogs.activefrog;
+        transform.position = col.ClosestPointOnBounds(curFrog.transform.position);
+    }
+
+}
