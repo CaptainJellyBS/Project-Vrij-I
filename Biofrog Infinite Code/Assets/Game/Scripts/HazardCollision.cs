e4cc1798d4efe4d4a3317fd1869e18e100735772
Mana <manakouwenberg@gmail.com>
2020-06-10 10:57:56 +0200

    Added collision with hazards in a roundabout way that makes me want to poke my eyes out, and changed some prefabs
    -Added tags to thorns and pipe prefabs
    -Added colliders to thorns and pipe prefabs
    -Added box trigger to frog

---
+++ Biofrog Infinite Code/Assets/Game/Scripts/HazardCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardCollision : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<Death>().Die();
+        }
+    }
+}
