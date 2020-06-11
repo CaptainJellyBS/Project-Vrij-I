de3c7294bd46fb0ab184ecfd927dddf500a41bbf
Mana <manakouwenberg@gmail.com>
2020-06-11 21:05:34 +0200

    Made a beginning for level 2, somehow still got inspiration and shit wtf

---
+++ Biofrog Infinite Code/Assets/Game/Scripts/FlickeringLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    public Light light;
+    public float maxIntensity;
+    public float minIntensity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(FlickerLight());
+    }
+
+    IEnumerator FlickerLight()
+    {
+        while(true)
+        {
+            light.intensity = minIntensity;
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            light.intensity = maxIntensity;
+
+            for (int r = Random.Range(2, 5); r >= 0; r--)
+            {
+                yield return new WaitForSeconds(Random.Range(0.03f, 0.15f));
+                light.intensity = minIntensity;
+                yield return new WaitForSeconds(Random.Range(0.03f, 0.15f));
+                light.intensity = maxIntensity;
+            }
+        }
+    }
+}
