a19bcd684952d89637589b4f43f3e7f7fc7f6e99
Mana <manakouwenberg@gmail.com>
2020-06-11 10:03:18 +0200

    Ignore raycasted some more models, swapped around the camera control buttons to make more sense

--- Biofrog Infinite Code/Assets/Game/Scripts/CameraFollowNew.cs
+++ Biofrog Infinite Code/Assets/Game/Scripts/CameraFollowNew.cs
@@ -41,38 +41,15 @@ public class CameraFollowNew : MonoBehaviour
 
     void HandleInput()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.Q))
         {
             transform.Rotate(Vector3.up, cameraRotateSpeed * Time.deltaTime, Space.World);
             offset = Quaternion.AngleAxis(cameraRotateSpeed * Time.deltaTime, Vector3.up) * offset;
         }
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.E))
         {
             transform.Rotate(Vector3.up, -cameraRotateSpeed * Time.deltaTime, Space.World);
             offset = Quaternion.AngleAxis(-cameraRotateSpeed * Time.deltaTime, Vector3.up) * offset;
         }
     }
-
-    //IEnumerator CameraRotation(float target)
-    //{
-    //    turning = true;
-    //    float i = 0;
-    //    float change = 2.5f;
-    //    while(i<target)
-    //    {
-    //        i += change;
-    //        transform.Rotate(Vector3.up, change, Space.World);
-    //        offset = Quaternion.AngleAxis(change, Vector3.up) * offset;
-    //        yield return null;
-    //    }
-
-    //    while (i > target)
-    //    {
-    //        i -= change;
-    //        transform.Rotate(Vector3.up, -change, Space.World);
-    //        offset = Quaternion.AngleAxis(-change, Vector3.up) * offset;
-    //        yield return null;
-    //    }
-    //    turning = false;
-    //}
 }
