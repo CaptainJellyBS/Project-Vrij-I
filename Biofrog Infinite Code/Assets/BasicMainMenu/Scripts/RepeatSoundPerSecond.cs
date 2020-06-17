8214ef6472479d039f3386c63afb6cf74d636651
Mana <manakouwenberg@gmail.com>
2020-06-17 18:13:11 +0200

    Added a volume slider to the main menu

---
+++ Biofrog Infinite Code/Assets/BasicMainMenu/Scripts/RepeatSoundPerSecond.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatSoundPerSecond : MonoBehaviour
+{
+    AudioSource audio;
+    public float secondsBetweenAudio = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        StartCoroutine(PlaySoundPerSecond());
+    }
+
+    IEnumerator PlaySoundPerSecond()
+    {
+        while(true)
+        {
+            audio.Play();
+            yield return new WaitForSeconds(secondsBetweenAudio);
+        }
+    }
+}
