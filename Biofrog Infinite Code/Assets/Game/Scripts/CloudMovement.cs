f33bc95ba199c382eff62419dac0792b31041308
Mana <manakouwenberg@gmail.com>
2020-06-17 14:58:58 +0200

    Added a cloud spawner and cloud prefab that makes Level 2 a tidbit more interesting

---
+++ Biofrog Infinite Code/Assets/Game/Scripts/CloudMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public Vector3 direction = Vector3.zero;
+    float maxDistance = float.PositiveInfinity;
+    float distanceFlown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float dist = speed * Time.deltaTime;
+        transform.position += direction * dist;
+        distanceFlown += dist;
+
+        if (distanceFlown >= maxDistance) { Destroy(gameObject); }
+    }
+
+    public void SetValues(float spd, Vector3 dir, float maxDis)
+    {
+        speed = spd; direction = dir; maxDistance = maxDis;
+    }
+}
---
+++ Biofrog Infinite Code/Assets/Game/Scripts/CloudSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public float cloudTimeMin, cloudTimeMax, cloudSpeedMin, cloudSpeedMax;
+    public float maxVarianceX, maxVarianceY, maxVarianceZ;
+    public Vector3 direction;
+    public float maxDistance;
+    public GameObject cloud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCloudCycle());   
+    }
+
+    IEnumerator SpawnCloudCycle()
+    {
+        while(true)
+        {
+            SpawnCloud();
+            yield return new WaitForSeconds(Random.Range(cloudTimeMin, cloudTimeMax));
+        }
+    }
+
+    void SpawnCloud()
+    {
+        Vector3 position = transform.position + new Vector3(Random.Range(-maxVarianceX, maxVarianceX), Random.Range(-maxVarianceY, maxVarianceY), Random.Range(-maxVarianceZ, maxVarianceZ));
+        GameObject newCloud = Instantiate(cloud, position, Quaternion.identity, transform);
+        newCloud.GetComponent<CloudMovement>().SetValues(Random.Range(cloudSpeedMin, cloudSpeedMax), direction, maxDistance);
+    }
+}
