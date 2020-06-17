440f1ddc11c29575d74261a977a5b2e918256b09
Mana <manakouwenberg@gmail.com>
2020-06-17 18:50:46 +0200

    Added a new game and continue function to the main menu

---
+++ Biofrog Infinite Code/Assets/BasicMainMenu/Scripts/LevelCounterThingy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCounterThingy : MonoBehaviour
+{
+    public int highestAchievedLevel = 0;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += CheckIfLevelLarger;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= CheckIfLevelLarger;
+    }
+
+    void CheckIfLevelLarger(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log(scene.buildIndex);
+        highestAchievedLevel = Mathf.Max(scene.buildIndex, highestAchievedLevel);
+    }
+}
